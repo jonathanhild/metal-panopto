3ace782d8275bfb44468fe47029e1baf63aed57a
Jonathan Hildenbrand <jonathan.hildenbrand@gmail.com>
2021-07-12 12:37:07 -0700

    update project name

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 # Copyright (C) 2021 Jonathan Hildenbrand
 #
-# This file is part of vargscore.
+# This file is part of VargScore.
 #
-# vargscore is free software: you can redistribute it and/or modify
+# VargScore is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# vargscore is distributed in the hope that it will be useful,
+# VargScore is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with vargscore.  If not, see <http://www.gnu.org/licenses/>.
+# along with VargScore.  If not, see <http://www.gnu.org/licenses/>.
 
 from flask import Flask
 
