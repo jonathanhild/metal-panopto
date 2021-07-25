9f886093f0d7f3e3d09eef3545f9e71af008a291
Jonathan Hildenbrand <jonathan.hildenbrand@gmail.com>
2021-07-24 21:14:06 -0700

    add model classes for full metallum data

--- src/database.py
+++ src/database.py
@@ -18,6 +18,7 @@
 from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 
 db = SQLAlchemy()
 
@@ -40,3 +41,124 @@ class SearchHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     search_text = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, default=datetime.now())
+
+
+class Band(db.Model):
+
+    __tablename__ = 'band'
+
+    id = db.Column(db.Integer, primary_key=True)
+    band_name = db.Column(db.Text)
+    country_of_origin = db.Column(db.Text)
+    location = db.Column(db.Text)
+    status = db.Column(db.Text)
+    formed_in = db.Column(db.Integer())
+    years_active = db.Column(db.Text)
+    lyrical_themes = db.Column(db.Text)
+    read_more_text = db.Column(db.Text)
+    current_label = db.relationship('Label', nullable=True)
+    discography = db.relationship('Album', nullable=True, backref='band')
+    members = db.relationship('Artist', nullable=True)
+    reviews = db.relationship('Review', nullable=True)
+    similar = db.relationship('BandSimilar', nullable=True, backref='band')
+    links = db.relationship('Link', nullable=True, backref='band')
+    timestamp = db.Column(db.DateTime, default=datetime.now())
+
+
+class Album(db.Model):
+
+    __tablename__ = 'album'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    type = db.Column(db.Text)
+    release_date = db.Column(db.Text)
+    catalog_id = db.Column(db.Text)
+    version_desc = db.Column(db.Text)
+    label = db.relationship('Label', backref='album')
+    format = db.Column(db.Text)
+    limitation = db.Column(db.Text)
+    additional_notes = db.Column(db.Text)
+    band_id = db.Column(db.Integer, db.ForeignKey('band.id'))
+
+
+class Song(db.Model):
+
+    __tablename__ = 'song'
+
+    id = db.Column(db.Integer, primary_key=True)
+    no = db.Column(db.Integer)
+    title = db.Column(db.Text)
+    length = db.Column(db.Text)
+    lyrics = db.Column(db.Text)
+
+
+class Artist(db.Model):
+
+    __tablename__ = 'artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    real_name = db.Column(db.Text)
+    age = db.Column(db.Text)
+    place_of_origin = db.Column(db.Text)
+    gender = db.Column(db.Text)
+    links = db.relationship('Link', backref='artist')
+
+
+class Review(db.Model):
+
+    __tablename__ = 'review'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    date = db.Column(db.DateTime)
+    based_on = db.Column(db.Text)
+    rating_pct = db.Column(db.Integer)
+    text = db.Column(db.Text)
+
+
+class BandSimilar(db.Model):
+
+    __tablename__ = 'band_similar'
+
+    id = db.Column(db.Integer, primary_key=True)
+    band_id = db.Column(db.Integer, db.ForeignKey('band.id'))
+    band_related_id = db.Column(db.Integer, db.ForeignKey('band.id'))
+
+
+class Link(db.Model):
+
+    __tablename__ = 'link'
+
+    id = db.Column(db.Integer, primary_key=True)
+    link = db.Column(db.Text)
+    link_url = db.Column(db.Text)
+
+
+class Label(db.Model):
+
+    __tablename__ = 'label'
+
+    id = db.Column(db.Integer, primary_key=True)
+    label_name = db.Column(db.Text)
+    address = db.Column(db.Text)
+    country = db.Column(db.Text)
+    phone_no = db.Column(db.Text)
+    status = db.Column(db.Text)
+    styles_specialities = db.Column(db.Text)
+    founding_date = db.Column(db.Text)
+    sub_labels = db.relationship('LabelSublabel')
+    online_shopping = db.Column(db.Text)
+    website = db.Column(db.Text)
+    email = db.Column(db.Text)
+    additional_notes = db.Column(db.Text)
+
+
+class LabelSublabel(db.Model):
+
+    __tablename__ = 'label_sublabel'
+
+    id = db.Column(db.Integer, primary_key=True)
+    label_id = db.Column(db.Integer, db.ForeignKey('label.id'))
+    sublabel_id = db.Column(db.Integer, db.ForeignKey('label.id'))
