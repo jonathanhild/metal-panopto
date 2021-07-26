2ac0f9242e52f8ce81c6c353c6b27402ab631e5b
Jonathan Hildenbrand <jonathan.hildenbrand@gmail.com>
2021-07-26 13:45:47 -0700

    remove unused import

--- src/metallum.py
+++ src/metallum.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with VargScore.  If not, see <http://www.gnu.org/licenses/>.
 
-from urllib.parse import urljoin, urlsplit
+from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
 
